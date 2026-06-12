36c024b37f6e23c17eba16b19481244aefa09587
Kieran McGee <rise14@gmail.com>
2026-06-11 23:15:17 -0400

    feat(single): show department banner above post title
    Add skeleton_wp_dept_banner() which looks up
    images/dept-banners/{category-slug}-banner.{jpg,jpeg,png} for the
    post's categories and prints the first match. Ships the Spotlight
    Album banner as the first one.

--- single.php
+++ single.php
@@ -15,6 +15,8 @@ get_header();
 
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
+            <?php skeleton_wp_dept_banner(); ?>
+
             <header class="entry-header">
                 <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
                 <?php
