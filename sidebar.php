2c46c1c9144bda2c957f954aabe03063988c4e2d
Kieran McGee <rise14@gmail.com>
2026-06-29 21:32:37 -0400

    feat(sidebar): add Mailchimp subscribe form at top of sidebar

--- sidebar.php
+++ sidebar.php
@@ -17,6 +17,29 @@
          DEEP ROOTS SIDEBAR CONTENT
     =========================== -->
 
+    <!-- Mailchimp Subscribe -->
+    <section class="widget widget-newsletter">
+        <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css">
+        <style type="text/css">
+            #mc_embed_signup{background:#fff;clear:left;font:14px Helvetica,Arial,sans-serif;width:100%;}
+        </style>
+        <div id="mc_embed_signup">
+            <form action="https://deeprootsmag.us11.list-manage.com/subscribe/post?u=e9f5b683830fc066840db426f&amp;id=ba09028be2&amp;f_id=0080aee0f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+                <div id="mc_embed_signup_scroll"><h2>Subscribe</h2>
+                    <div class="mc-field-group"><label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label><input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required value=""></div>
+                    <div id="mce-responses" class="clear">
+                        <div class="response" id="mce-error-response" style="display:none;"></div>
+                        <div class="response" id="mce-success-response" style="display:none;"></div>
+                    </div>
+                    <div style="position:absolute;left:-5000px;" aria-hidden="true"><input type="text" name="b_e9f5b683830fc066840db426f_ba09028be2" tabindex="-1" value=""></div>
+                    <div class="clear"><input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button" value="Subscribe"></div>
+                </div>
+            </form>
+        </div>
+        <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
+        <script type="text/javascript">(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+    </section>
+
     <?php /* Newsletter Subscribe — listmonk form disabled
     <section class="widget widget-newsletter">
         <h3 class="widget-title"><?php esc_html_e( 'Subscribe!', 'skeleton-wp' ); ?></h3>
