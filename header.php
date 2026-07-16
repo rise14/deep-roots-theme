b7700d367361c9f912ae46d05e5d256edf7c7760
Kieran McGee <rise14@gmail.com>
2026-07-15 23:45:08 -0400

    feat(analytics): add Ahrefs analytics tag to head

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="profile" href="https://gmpg.org/xfn/11">
+    <script src="https://analytics.ahrefs.com/analytics.js" data-key="EsbEeFWs6DFJnLfzVnXQDQ" async></script>
     <?php wp_head(); ?>
 </head>
 
